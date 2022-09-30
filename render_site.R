3f1e8585e0bc88ca694be5783d3b9c0ab1d38006
kpgund <kpgund>
2022-09-30 13:39:50 -0400

    Updating website

---
+++ render_site.R
@@ -0,0 +1,10 @@
+# install.packages("include_graphics")
+library(rmarkdown)
+library(knitr)
+
+rmarkdown::render_site(encoding = "UTF-8")
+
+
+## sources:
+## https://rstudio4edu.github.io/rstudio4edu-book/rmd-fancy.html#anatomy-of-a-fancy-r-markdown-page
+## http://jsfiddle.net/KQFBb/1/
